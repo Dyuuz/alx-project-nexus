61018aaf1d90c430c4b2bb340e53c0acda8c5e22
Dyuuz <lekanoyesunle@gmail.com>
2026-01-28 00:26:33 +0100

    update: django order model

--- ecom/orders/models.py
+++ ecom/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
+from payment.models import Payment
 
-# Create your models here.
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    payment = models.ForeignKey(
+        Payment,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders"
+    )
+    payment_status = models.CharField(max_length=10, default="pending")
+    status = models.CharField(max_length=8, default="hold")
+    date = models.DateTimeField(auto_now_add=True)
