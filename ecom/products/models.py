4697053dc48d5f54ea215f4841e4eaed3821cde0
Dyuuz <lekanoyesunle@gmail.com>
2026-01-27 19:59:30 +0100

    update: django product model

--- ecom/products/models.py
+++ ecom/products/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from accounts.models import CustomUser
+from django.utils.text import slugify
+from django.core.validators import MinValueValidator, MaxValueValidator
+import uuid
 
 # Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='images/', blank=True)
+    slug = models.SlugField(max_length=50, unique=True, db_index=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True) 
+
+    def save(self, *args, **kwargs):
+        if self.name:
+            self.name = self.name.title()
+        if not self.slug:
+            self.slug = self.create_slug_for_category
+        super().save(*args, **kwargs)
+
+    @property
+    def create_slug_for_category(self):
+        baseURL = slugify(self.name)
+        slug = baseURL
+        counter = 1
+        while Category.objects.filter(slug=slug).exists():
+            slug = f"{baseURL}-{counter}"
+            counter += 1
+        return slug
+    
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    slug = models.SlugField(max_length=50, unique=True, db_index=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
+    vendor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='products')
+    image = models.ImageField(upload_to='images/', blank=True)
+    public_id = models.CharField(blank=True)
+    srcURL = models.URLField(blank=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    stock = models.PositiveIntegerField(default=0)
+    original_price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_percent = models.PositiveIntegerField(
+                    validators=[MinValueValidator(0), MaxValueValidator(70)],
+                    default=0, help_text="Discount percentage (0-70).", null=True, blank=True
+    )
+    discount_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        null=True, blank=True
+    )
+    date_added = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        # Calculate discount amount if discount percent is set
+        if self.name:
+            self.name = self.name.title().strip()
+        if self.discount_percent != 0:
+            self.discount_amount = self.original_price * (self.discount_percent / 100)
+        if not self.slug:
+            self.slug = self.create_slug_for_product
+        super().save(*args, **kwargs)
+
+    @property
+    def create_slug_for_product(self):
+        baseURL = slugify(self.name)
+        slug = baseURL
+        counter = 1
+        while Product.objects.filter(slug=slug).exists():
+            slug = f"{baseURL}-{counter}"
+            counter += 1
+        return slug    
