e0a4c9a9615a9f60744a9d3a240338c212b06794
Dyuuz <lekanoyesunle@gmail.com>
2026-02-03 20:34:14 +0100

    update: admin models UI

--- ecom/accounts/admin.py
+++ ecom/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Vendor, CustomUser, BankAccount, AuthSession
 
 # Register your models here.
+admin.site.register(Vendor)
+admin.site.register(CustomUser)
+admin.site.register(BankAccount)
+# admin.site.register(AuthSession)
