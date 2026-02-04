3e4f16418a98fea3317b476f2abb6c50d627a3f0
Dyuuz <lekanoyesunle@gmail.com>
2026-02-04 08:39:46 +0100

    update: product migrations

--- ecom/products/models.py
+++ ecom/products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models import CustomUser
+from accounts.models import Vendor
 from django.utils.text import slugify
 from django.core.validators import MinValueValidator, MaxValueValidator
 import uuid
@@ -35,7 +35,7 @@ class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     slug = models.SlugField(max_length=50, unique=True, db_index=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
-    vendor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='products')
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='products')
     image = models.ImageField(upload_to='images/', blank=True)
     public_id = models.CharField(blank=True)
     srcURL = models.URLField(blank=True)
