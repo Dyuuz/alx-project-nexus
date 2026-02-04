c297a5f69e1a1d05ecc54df3d355430bbfa4729c
Dyuuz <lekanoyesunle@gmail.com>
2026-02-04 08:53:59 +0100

    update: tests template

---
+++ ecom/products/tests/test_category.py
@@ -0,0 +1,5 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from products.models import Category, Product
+
---
+++ ecom/products/tests/test_products.py
@@ -0,0 +1,4 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from products.models import Category, Product
