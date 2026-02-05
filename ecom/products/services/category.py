65f4dd2478cf572473b385481fab39d7c1a6b8c5
Dyuuz <lekanoyesunle@gmail.com>
2026-02-05 18:34:23 +0100

    update: product category service

--- ecom/products/services/category.py
+++ ecom/products/services/category.py
@@ -1,3 +1,20 @@
 from django.db import transaction
 from products.models import Category
-from rest_framework.exceptions import ValidationError
+
+
+@transaction.atomic
+def create_category(data: dict) -> Category:
+    return Category.objects.create(**data)
+
+
+@transaction.atomic
+def update_category(category: Category, data: dict) -> Category:
+    for field, value in data.items():
+        setattr(category, field, value)
+    category.save()
+    return category
+
+
+@transaction.atomic
+def delete_category(category: Category) -> None:
+    category.delete()
