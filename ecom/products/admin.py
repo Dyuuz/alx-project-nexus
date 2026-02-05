42f6c365a145851e6b5e80f711dc3fdeb62f137f
Dyuuz <lekanoyesunle@gmail.com>
2026-02-05 16:41:48 +0100

    update: products admin to support save services and not only on API Calls

--- ecom/products/admin.py
+++ ecom/products/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from products.models import Product
+from products.services.products import (
+    create_product,
+    update_product,
+    delete_product,
+)
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "vendor",
+        "original_price",
+        "discount_percent",
+        "discount_amount",
+        "stock",
+        "created_at"
+    )
+    list_filter = ("vendor", "category")
+    search_fields = ("name", "vendor__business_name")
+
+    def save_model(self, request, obj, form, change):
+        """
+        Force Django admin to use the ProductService
+        instead of calling obj.save() directly.
+        """
+        data = form.cleaned_data
+
+        if change:
+            update_product(obj.id, **data)
+        else:
+            create_product(**data)
+
+    def delete_model(self, request, obj):
+        delete_product(obj)
