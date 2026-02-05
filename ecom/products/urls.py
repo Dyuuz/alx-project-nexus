a4d56928b4095f89320f9e4cb8e007d98aee68a3
Dyuuz <lekanoyesunle@gmail.com>
2026-02-05 18:36:39 +0100

    update: products urls

--- ecom/products/urls.py
+++ ecom/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from products.views.products import ProductViewSet
+from products.views.category import CategoryViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register("products", ProductViewSet, basename="product")
+router.register("categories", CategoryViewSet, basename="category")
 
 urlpatterns = router.urls
