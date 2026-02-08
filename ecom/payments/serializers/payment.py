ebc1c140ca43bab4f08132f42396cf3d77c32b06
Dyuuz <lekanoyesunle@gmail.com>
2026-02-08 23:44:35 +0100

    update: payments serializes docs

--- ecom/payments/serializers/payment.py
+++ ecom/payments/serializers/payment.py
@@ -3,6 +3,13 @@ from payments.models import Payment
 
 
 class PaymentReadSerializer(serializers.ModelSerializer):
+    """
+    Serializer for reading payment details.
+
+    Returns payment information related to an order.
+    All fields are read-only.
+    """
+
     class Meta:
         model = Payment
         fields = ("id", "order", "amount", "provider", "reference", "status", "created_at")
@@ -10,10 +17,17 @@ class PaymentReadSerializer(serializers.ModelSerializer):
 
 
 class PaymentInitiateSerializer(serializers.Serializer):
+    """
+    Validates data required to initiate a payment.
+    """
+    
     order_id = serializers.UUIDField(required=True)
     provider = serializers.CharField(required=False, default="internal")
 
 
 class PaymentConfirmSerializer(serializers.Serializer):
+    """
+    Validates the payment reference used to confirm a payment.
+    """
     reference = serializers.CharField(required=True)
     
