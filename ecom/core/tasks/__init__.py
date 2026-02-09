0893e954e00c3a578555e2ee73d6ee61296be085
Dyuuz <lekanoyesunle@gmail.com>
2026-02-09 21:28:00 +0100

    add: initialize init and cart task

---
+++ ecom/core/tasks/__init__.py
@@ -0,0 +1,1 @@
+from core.tasks.carts import cleanup_abandoned_carts 
--- ecom/core/tasks/carts.py
+++ ecom/core/tasks/carts.py
@@ -1,20 +1,27 @@
 from cart.models import Cart
-from time import timezone
+from django.utils import timezone
 from datetime import timedelta
 from celery import shared_task
 from django.conf import settings
 from cart.services.cart import CartService
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Cart Tasks
 
-@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=30, retry_kwargs={"max_retries": 3})
-def cleanup_abandoned_carts(ttl_minutes=settings.CART_TTL_MINUTES):
+@shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={"max_retries": 3, "countdown": 45})
+def cleanup_abandoned_carts(self):
+    ttl_minutes = settings.CART_TTL_MINUTES
     expiry_time = timezone.now() - timedelta(minutes=ttl_minutes)
+    logger.info("Cart cleanup expiry time: %s", expiry_time)
 
     carts = Cart.objects.filter(
         status__in=("unpaid", "pending"),
         last_activity_at__lt=expiry_time,
     )
-
-    for cart in carts.iterator():
-        CartService.expire_cart(cart, reason="Cart inactive beyond TTL")
+    
+    if carts:
+        for cart in carts.iterator():
+            logger.info("Expiring cart %s with status %s", cart.id, cart.status)
+            CartService.expire_cart(cart, reason="Cart inactive beyond TTL")
