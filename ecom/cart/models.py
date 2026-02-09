d9a322bd0f2da89a73339ab361b17384afbecbbc
Dyuuz <lekanoyesunle@gmail.com>
2026-02-09 19:21:32 +0100

    update: model Checkout String

--- ecom/cart/models.py
+++ ecom/cart/models.py
@@ -106,3 +106,6 @@ class Checkout(models.Model):
     payment_method = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Checkout {self.id} - {self.cart.status}"
