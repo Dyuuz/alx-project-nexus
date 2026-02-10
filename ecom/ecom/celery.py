832e96f3174e3d8fd77ff1dcbdaf34b8c5e6cab1
Dyuuz <lekanoyesunle@gmail.com>
2026-02-10 23:33:40 +0100

    update: new celery background jobs

--- ecom/ecom/celery.py
+++ ecom/ecom/celery.py
@@ -12,8 +12,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom.settings')
 app = Celery('ecom')
 
 # Use Redis as broker and backend
-app.conf.broker_url = os.getenv("REDIS_URL", "redis://localhost:637/0")
-app.conf.result_backend = os.getenv("REDIS_URL", "redis://localhost:6379/1")
+app.conf.broker_url = os.getenv("REDIS_URL")
+app.conf.result_backend = os.getenv("REDIS_URL")
 
 # Load Django settings (CELERY_ namespace)
 app.config_from_object('django.conf:settings', namespace='CELERY')
@@ -46,8 +46,28 @@ def debug_task(self):
 
 # CELERY BEAT SCHEDULES
 app.conf.beat_schedule = {
-    "Clean-abandoned-carts": {
-        "task": "core.tasks.carts.cleanup_abandoned_carts",
+    # "Cancel-unpaid-order": {
+    #     "task": "core.tasks.cancel_unpaid_orders_task",
+    #     "schedule": timedelta(minutes=1),
+    # },
+    "Cancel-unpaid-order": {
+        "task": "core.tasks.cancel_unpaid_orders_task",
         "schedule": timedelta(minutes=1),
     },
+    # "Expire-pending-checkouts": {
+    #     "task": "core.tasks.expire_pending_checkouts_task",
+    #     "schedule": timedelta(minutes=1),
+    # },
+    # "Clean-abandoned-carts": {
+    #     "task": "core.tasks.cleanup_carts_task",
+    #     "schedule": timedelta(minutes=1),
+    # },
+    # "Send-payment-reminder-24hrs": {
+    #     "task": "core.tasks.send_payment_reminder_24htask",
+    #     "schedule": timedelta(minutes=1),
+    # },
+    # "Send-final-payment-reminder": {
+    #     "task": "core.tasks.send_final_payment_reminder_task",
+    #     "schedule": timedelta(minutes=1),
+    # },
 }
