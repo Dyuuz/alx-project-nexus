c01e84a0445db2615c9d0c0a0521374c4316e233
Dyuuz <lekanoyesunle@gmail.com>
2026-02-11 11:41:43 +0100

    update: setting Debug state

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -32,7 +32,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG"))
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
