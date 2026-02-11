654326963bdd9c9723755abfd4ccc0f065d31728
Lekan Oyesunle <lekanoyesunle@gmail.com>
2026-02-11 10:49:45 +0100

    update: create superuser on deploy

--- ecom/start.sh
+++ ecom/start.sh
@@ -13,6 +13,30 @@ echo "=== Step 1/3: applying migrations ==="
 python manage.py migrate --noinput
 echo "=== Migrations: done ==="
 
+echo "=== Step 1.5/3: creating superuser if not exists ==="
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if email and password:
+    if not User.objects.filter(email=email).exists():
+        print("Creating superuser...")
+        User.objects.create_superuser(
+            email=email,
+            password=password
+        )
+    else:
+        print("Superuser already exists.")
+else:
+    print("Superuser credentials not provided.")
+END
+
 echo "=== Step 2/3: collecting static files ==="
 python manage.py collectstatic --noinput -v 2
 echo "=== Collectstatic: done ==="
