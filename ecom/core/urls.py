8d66f8fbde42d78e6a1e6c483fae36fcc3970b8a
Dyuuz <lekanoyesunle@gmail.com>
2026-02-18 11:15:30 +0100

    update: sentry test in prod

--- ecom/core/urls.py
+++ ecom/core/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def sentry_test(request):
+    1 / 0
+    return Response({"ok": True})
+
 urlpatterns = [
-    
+    path("sentry-test/", sentry_test),
 ]
