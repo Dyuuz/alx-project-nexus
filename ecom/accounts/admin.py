713670f76441a3ef223af43e7bb6cf6c30610c97
Dyuuz <lekanoyesunle@gmail.com>
2026-02-25 15:47:08 +0100

    rem: admin create/update using account custom service

--- ecom/accounts/admin.py
+++ ecom/accounts/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Vendor, CustomUser, BankAccount, AuthSession
-from accounts.services.user_service import create_user, update_user
 
 # Register your models here.
 admin.site.register(Vendor)
@@ -19,19 +18,4 @@ class CustomUserAdmin(admin.ModelAdmin):
         "created_at",
     )
     search_fields = ("email", "first_name", "last_name")
-
     ordering = ("-created_at",)
-    
-    def save_model(self, request, obj, form, change):
-        """
-        Route admin updates through the user service
-        to enforce system-level rules.
-        """
-        data = form.cleaned_data
-
-        if change:
-            # Update existing user via service
-            update_user(obj, data)
-        else:
-            # Creating a user — let Django handle it
-            super().save_model(request, obj, form, change)
