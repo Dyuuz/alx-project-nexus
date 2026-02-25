54a60513fc808ebfa3e4d0169f9b84454923627a
Dyuuz <lekanoyesunle@gmail.com>
2026-02-25 15:48:11 +0100

    update: AuthTokenViewSet and PasswordResetViewSet

--- ecom/accounts/urls.py
+++ ecom/accounts/urls.py
@@ -1,10 +1,15 @@
 from rest_framework.routers import DefaultRouter
-from accounts.views.user import UserViewSet, LoginViewSet, EmailVerificationViewSet
+from accounts.views.user import (
+    UserViewSet, LoginViewSet, EmailVerificationViewSet,
+    AuthTokenViewSet, PasswordResetViewSet
+)
 from accounts.views.vendor import VendorViewSet
 from accounts.views.bank import BankAccountViewSet
 
 router = DefaultRouter()
 router.register("auth", EmailVerificationViewSet, basename="auth")
+router.register("auth", AuthTokenViewSet, basename="auth-token")
+router.register("auth", PasswordResetViewSet, basename="auth-password-reset")
 router.register("auth/users", UserViewSet, basename="user")
 router.register("login", LoginViewSet, basename="login")
 router.register("vendors", VendorViewSet, basename="vendor")
