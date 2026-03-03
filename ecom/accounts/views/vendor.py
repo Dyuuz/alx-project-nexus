6e34508d930d750dbc031aff422872471798b366
Dyuuz <lekanoyesunle@gmail.com>
2026-03-03 19:24:52 +0100

    update: vendor update permsiions

--- ecom/accounts/views/vendor.py
+++ ecom/accounts/views/vendor.py
@@ -93,7 +93,7 @@ class VendorViewSet(ModelViewSet):
             return [IsAuthenticated(), IsEmailVerified()]
 
         if self.action in ["list", "update", "partial_update"]:
-            return [IsAuthenticated(), IsAdminOrSelf(), IsEmailVerified(),  ()]
+            return [IsAuthenticated(), IsAdminOrSelf(), IsEmailVerified(), IsVendor()]
 
         if self.action == "destroy":
             return [IsAuthenticated(), IsAdmin()]
