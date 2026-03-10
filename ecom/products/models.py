c4b9ef0c8e38fc7d26240fd4405682095eb083c0
Dyuuz <lekanoyesunle@gmail.com>
2026-03-10 23:56:42 +0100

    update: model category ordering

--- ecom/products/models.py
+++ ecom/products/models.py
@@ -16,6 +16,9 @@ class Category(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True) 
     
+    class Meta:
+        ordering = ["name"]
+    
     def __str__(self):
         return self.name
 
